9d5a6e77edcf3f70d06cd96a15b63974db920834
zacharybaca <bacazach@icloud.com>
2021-07-24 14:12:10 -0500

    fixed syntax issue

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   searchUsers = async text => {
     this.setState({ loading: true })
-    const res = await axios.get('https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}')
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
 
     this.setState({ users: res.data.items, loading: false})
   };
