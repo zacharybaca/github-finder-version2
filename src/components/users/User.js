2f552e9e8312ce4acaf1a70b500bff1217cb2f02
zacharybaca <bacazach@icloud.com>
2021-08-02 00:15:09 -0500

    converted User to functional component with hooks

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,25 +1,19 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect  } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 
-class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+        // eslint-disable-next-line
+    }, []);
+    
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired
-    };
 
-    render() {
         const {
             name,
             avatar_url,
@@ -34,9 +28,9 @@ class User extends Component {
             public_repos,
             public_gists,
             hireable
-        } = this.props.user;
+        } = user;
 
-        const { loading, repos } = this.props;
+        
 
         if (loading) return <Spinner />
 
@@ -89,7 +83,14 @@ class User extends Component {
                 <Repos repos={repos} />  
             </Fragment>
         )
-    }
 }
+
+User.propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
+};
 
 export default User
