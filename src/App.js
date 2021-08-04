86643fa61d9fbd638faef4ea6770e7dc25ba0933
zacharybaca <bacazach@icloud.com>
2021-08-03 21:28:34 -0500

    added search users into github context

--- src/App.js
+++ src/App.js
@@ -18,13 +18,7 @@ const App = () => {
   const [alert, setAlert] = useState(null);
   
 
-  const searchUsers = async text => {
-    setLoading(true);
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
+  
 
   const getUser = async (username) => {
     setLoading(true);
@@ -65,8 +59,7 @@ const App = () => {
           <Switch>
             <Route exact path='/' render={props => (
               <Fragment>
-                <Search 
-                  searchUsers={searchUsers} 
+                <Search  
                   clearUsers={clearUsers} 
                   showClear={users.length > 0 ? true: false} 
                   setAlert={showAlert}
