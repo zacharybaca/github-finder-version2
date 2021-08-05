bf6a1fac3a8f69a0052bf3452acdffad969799cc
zacharybaca <bacazach@icloud.com>
2021-08-05 03:02:15 -0500

    added alert state to context

--- src/App.js
+++ src/App.js
@@ -4,33 +4,27 @@ import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
   
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-
-    setTimeout(() => setAlert(null), 5000);
-  };
 
     return (
       <GithubState>
+      <AlertState>
       <Router>
       <div className="App">
         <Navbar />
         <div className='container'>
-          <Alert alert={alert} />
+          <Alert />
           <Switch>
             <Route exact path='/' render={props => (
               <Fragment>
-                <Search   
-                  setAlert={showAlert}
-            />
+                <Search />
                 <Users />
               </Fragment>
             )} />
@@ -41,6 +35,7 @@ const App = () => {
         </div>
       </div>
       </Router>
+      </AlertState>
       </GithubState>
     );
   
